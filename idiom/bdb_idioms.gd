969f86b6c43a07d89b0a5801b685c4301b629075
Accidentallyc <project.cassey@gmail.com>
2025-10-06 00:02:36 +1300

    New BdbSig

--- idiom/bdb_idioms.gd
+++ idiom/bdb_idioms.gd
@@ -5,7 +5,3 @@ class_name BdbIdioms
 static func free_children(parent:Node):
 	for c in parent.get_children():
 		c.free()
-
-static func clear_signal(sig:Signal):
-	for connection in sig.get_connections():
-		sig.disconnect(connection.callable)
